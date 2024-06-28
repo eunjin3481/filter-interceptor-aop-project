892488b704c243e1743733db2b127a8418b52252
EunJin Shin <eunjin3481@gmail.com>
2024-06-28 14:51:03 +0900

    create project

---
+++ src/main/java/org/example/userproject/vo/User.java
@@ -0,0 +1,12 @@
+package org.example.userproject.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    String id;
+    String password;
+    String name;
+}
